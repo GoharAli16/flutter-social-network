5f6e0b5645f7ed8e2c004a107d8f5a28539bfd5f
Gohar Ali <goharali16@gmail.com>
2023-07-20 09:15:00 +0500

    Code formatting and optimization
    - Clean up main.dart formatting
    - Optimize service initialization
    - Improve code readability and consistency
    - Update dependency management

--- lib/main.dart
+++ lib/main.dart
@@ -17,23 +17,19 @@ import 'features/stories/presentation/providers/stories_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
+
   // Initialize Firebase
   await Firebase.initializeApp();
-  
+
   // Initialize Hive
   await Hive.initFlutter();
-  
+
   // Initialize services
   await NotificationService.initialize();
   await AnalyticsService.initialize();
   await RealtimeService.initialize();
-  
-  runApp(
-    const ProviderScope(
-      child: SocialNetworkApp(),
-    ),
-  );
+
+  runApp(const ProviderScope(child: SocialNetworkApp()));
 }
 
 class SocialNetworkApp extends ConsumerWidget {
@@ -43,7 +39,7 @@ class SocialNetworkApp extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final router = ref.watch(routerProvider);
     final themeMode = ref.watch(themeModeProvider);
-    
+
     return MaterialApp.router(
       title: AppConfig.appName,
       debugShowCheckedModeBanner: false,
