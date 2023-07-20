43a15e72d0fb37f0c2ff167c5134f9acf6be82b7
Gohar Ali <goharali16@gmail.com>
2023-07-20 09:15:00 +0500

    Initial setup: Advanced Flutter social media app
    - Create comprehensive social networking platform with modern architecture
    - Set up Riverpod state management and real-time features
    - Add Firebase authentication and Firestore database integration
    - Implement real-time messaging with Socket.IO and WebSocket
    - Configure live streaming with Agora RTC engine
    - Add stories feature with 24-hour expiration
    - Include AI-powered content recommendations and moderation
    - Set up push notifications and analytics tracking
    
    Core Features:
    - Real-time messaging and group chats
    - Stories with creative tools and filters
    - Live streaming and video calls
    - AI content moderation and recommendations
    - Advanced privacy controls
    - Multi-media sharing (photos, videos, audio)
    - Location-based features
    - Social discovery and friend suggestions

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'core/config/app_config.dart';
+import 'core/theme/app_theme.dart';
+import 'core/routing/app_router.dart';
+import 'core/services/notification_service.dart';
+import 'core/services/analytics_service.dart';
+import 'core/services/realtime_service.dart';
+import 'features/auth/presentation/providers/auth_provider.dart';
+import 'features/feed/presentation/providers/feed_provider.dart';
+import 'features/messaging/presentation/providers/messaging_provider.dart';
+import 'features/stories/presentation/providers/stories_provider.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  // Initialize Firebase
+  await Firebase.initializeApp();
+  
+  // Initialize Hive
+  await Hive.initFlutter();
+  
+  // Initialize services
+  await NotificationService.initialize();
+  await AnalyticsService.initialize();
+  await RealtimeService.initialize();
+  
+  runApp(
+    const ProviderScope(
+      child: SocialNetworkApp(),
+    ),
+  );
+}
+
+class SocialNetworkApp extends ConsumerWidget {
+  const SocialNetworkApp({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final router = ref.watch(routerProvider);
+    final themeMode = ref.watch(themeModeProvider);
+    
+    return MaterialApp.router(
+      title: AppConfig.appName,
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: themeMode,
+      routerConfig: router,
+    );
+  }
+}
